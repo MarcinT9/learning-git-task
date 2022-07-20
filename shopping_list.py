0909d546f7399ec4c9fb369bdaad9f1b8c883e8d
Marcin Topczewski <marcintopczewski90@gmail.com>
2022-07-20 11:23:44 +0200

    Add rest code

--- shopping_list.py
+++ shopping_list.py
@@ -4,3 +4,10 @@ shopping_list = {
     'piekarnia': ['bułka', 'chleb', 'pączek'],
     'warzywniak': ['marchew', 'seler', 'ziemniaki']
 }
+for shop, product in shopping_list.items():
+    print(f'Idę do {shop.capitalize()} i kupuję tam {product}')
+
+
+element = shopping_list['piekarnia'] + shopping_list['warzywniak']
+
+print(f'W sumie kupuję {len(element)} produktów.')
