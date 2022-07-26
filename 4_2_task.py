c977a475b5cc7d8e619c84e8f92d0c89ee76e12f
Marcin Topczewski <marcintopczewski90@gmail.com>
2022-07-26 20:45:26 +0200

    Add documentation to file

--- 4_2_task.py
+++ 4_2_task.py
@@ -1,4 +1,8 @@
 def is_palindrome(word):
+    """
+    Function is_palindrome based on string arguments passed.
+    Checks if the characters in the string are the same from left to right as from right to left.
+    """
     if word[::-1] == word:
         return True
     else:
@@ -7,3 +11,6 @@ def is_palindrome(word):
 print(is_palindrome('kajak'))
 print(is_palindrome('Marcin'))
 print(is_palindrome('malajalam'))
+print(is_palindrome('565'))
+print(is_palindrome('712'))
+help(is_palindrome)
